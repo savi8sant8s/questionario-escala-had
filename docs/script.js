7993969b53b8562ca9aaf94394a33d6e767ee2f4
unknown <edegleyson.2013@gmail.com>
2022-04-18 23:53:25 -0300

    Adicionado questoes, resposta e valores

--- docs/script.js
+++ docs/script.js
@@ -4,21 +4,129 @@ const app = new Vue({
         return {
             questoes: [
                 {
-                    pergunta: 'O que você faz?',
+                    pergunta: 'Eu me sinto tensa(o) ou contraída(o):',
                     alternativas: [
-                        { descricao: 'Faz nada', valor: 1 },
-                        { descricao: 'Faz algo', valor: 2 },
-                        { descricao: 'Faz outra coisa', valor: 3 },
-                        { descricao: 'Faz muito', valor: 4 }
+                        { descricao: ' a maior parte do tempo', valor: 3 },
+                        { descricao: 'boa parte do tempo', valor: 2 },
+                        { descricao: 'de vez em quando', valor: 1 },
+                        { descricao: 'nunca', valor: 0 }
                     ],
                 },
                 {
-                    pergunta: 'O que você não faz?',
+                    pergunta: 'Eu ainda sinto que gosto das mesmas coisas de antes:',
                     alternativas: [
-                        { descricao: 'Faz tudo', valor: 1 },
-                        { descricao: 'Faz cuscuz', valor: 2},
-                        { descricao: 'Faz comida', valor: 3},
-                        { descricao: 'Faz quase nada', valor: 4}
+                        { descricao: 'sim, do mesmo jeito que antes', valor: 0 },
+                        { descricao: 'não tanto quanto antes', valor: 1},
+                        { descricao: 'só um pouco', valor: 2},
+                        { descricao: 'já não consigo ter prazer em nada', valor: 3}
+                    ],
+                },
+                {
+                    pergunta: 'Eu sinto uma espécie de medo, como se alguma coisa ruim fosse acontecer:',
+                    alternativas: [
+                        { descricao: 'sim, de jeito muito forte', valor: 3 },
+                        { descricao: 'sim, mas não tão forte', valor: 2},
+                        { descricao: 'um pouco, mas isso não me preocupa', valor: 1},
+                        { descricao: 'não sinto nada disso', valor: 0}
+                    ],
+                },
+                {
+                    pergunta: 'Dou risada e me divirto quando vejo coisas engraçadas:',
+                    alternativas: [
+                        { descricao: 'do mesmo jeito que antes', valor: 0 },
+                        { descricao: ' atualmente um pouco menos', valor: 1},
+                        { descricao: 'atualmente bem  menos', valor: 2},
+                        { descricao: 'não consigo mais', valor: 3}
+                    ],
+                },
+                {
+                    pergunta: 'Estou com a cabeça cheia de preocupações:',
+                    alternativas: [
+                        { descricao: 'a maior parte do tempo', valor: 3 },
+                        { descricao: 'boa parte do tempo', valor: 2},
+                        { descricao: 'de vez em quando', valor: 1},
+                        { descricao: 'raramente', valor: 0}
+                    ],
+                },
+                {
+                    pergunta: 'Eu me sinto alegre:',
+                    alternativas: [
+                        { descricao: 'nunca', valor: 3 },
+                        { descricao: 'poucas vezes', valor: 2},
+                        { descricao: 'muitas vezes', valor: 1},
+                        { descricao: 'a maior parte do tempo', valor: 0}
+                    ],
+                },
+                {
+                    pergunta: 'Consigo ficar sentado à vontade e me sentir relaxada(o):',
+                    alternativas: [
+                        { descricao: 'sim, quase sempre', valor: 0 },
+                        { descricao: 'muitas vezes', valor: 1},
+                        { descricao: 'poucas vezes', valor: 2},
+                        { descricao: 'nunca', valor: 3}
+                    ],
+                },
+                {
+                    pergunta: 'Eu estou lenta(o) para pensar e fazer coisas:',
+                    alternativas: [
+                        { descricao: 'quase sempre', valor: 3 },
+                        { descricao: 'muitas vezes', valor: 2},
+                        { descricao: 'poucas vezes', valor: 1},
+                        { descricao: 'nunca', valor: 0}
+                    ],
+                },
+                {
+                    pergunta: 'Eu tenho uma sensação ruim de medo, como um frio na barriga ou um aperto no estômago:',
+                    alternativas: [
+                        { descricao: 'nunca', valor: 0 },
+                        { descricao: 'de vez em quando', valor: 1},
+                        { descricao: 'muitas vezes', valor: 2},
+                        { descricao: 'quase sempre', valor: 3}
+                    ],
+                },
+                {
+                    pergunta: 'Eu perdi o interesse em cuidar da minha aparência:',
+                    alternativas: [
+                        { descricao: 'completamente', valor: 3 },
+                        { descricao: 'não estou mais me cuidando como eu deveria', valor: 2},
+                        { descricao: 'talvez não tanto quanto antes', valor: 1},
+                        { descricao: ' me cuido do mesmo jeito que antes', valor: 0}
+                    ],
+                },
+                {
+                    pergunta: 'Eu me sinto inquieta(o), como se eu não pudesse ficar parada(o) em lugar nenhum:',
+                    alternativas: [
+                        { descricao: 'sim, demais', valor: 3 },
+                        { descricao: 'bastante', valor: 2},
+                        { descricao: 'um pouco', valor: 1},
+                        { descricao: 'não me sinto assim', valor: 0}
+                    ],
+                },
+                {
+                    pergunta: 'Fico animada(o) esperando animado as coisas boas que estão por vir:',
+                    alternativas: [
+                        { descricao: 'do mesmo jeito que antes', valor: 0 },
+                        { descricao: 'um pouco menos que antes', valor: 1},
+                        { descricao: 'bem menos do que antes', valor: 2},
+                        { descricao: 'quase nunca', valor: 3}
+                    ],
+                },
+                {
+                    pergunta: 'De repente, tenho a sensação de entrar em pânico:',
+                    alternativas: [
+                        { descricao: 'a quase todo momento', valor: 3 },
+                        { descricao: 'várias vezes', valor: 2},
+                        { descricao: 'de vez em quando', valor: 1},
+                        { descricao: 'não senti isso', valor: 0}
+                    ],
+                },
+                {
+                    pergunta: 'Consigo sentir prazer quando assisto a um bom programa de televisão, de rádio ou quando leio alguma coisa:',
+                    alternativas: [
+                        { descricao: 'quase sempre', valor: 0 },
+                        { descricao: 'várias vezes', valor: 1},
+                        { descricao: 'poucas vezes', valor: 2},
+                        { descricao: 'quase nunca', valor: 3}
                     ],
                 }
             ],
